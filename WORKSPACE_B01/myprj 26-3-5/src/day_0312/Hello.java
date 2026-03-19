0bf18a93e69f99358b41e73662e0b8c3df63ff55
ksh1227 <ksh12@student.hywoman.ac.kr>
2026-03-19 14:41:17 +0900

    0319 커밋

---
+++ WORKSPACE_B01/myprj 26-3-5/src/day_0312/Hello.java
@@ -0,0 +1,27 @@
+package day_0312;
+
+public class Hello {
+	//클래스의 구성 요소 = 데이터(멤버 변수) 와 메소드(멤버 메소드)
+	
+	static String name = "김승현";
+	
+	public static int sum(int n, int m){
+		return n + m ;
+	}
+	
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '?';
+		
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+		System.out.println(name);
+		//복붙 컨트롤+알트+방향기 - 이동 알트+방향키
+	}
+
+}
